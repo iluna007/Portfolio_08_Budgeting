096e0ff3ef735e7124d38f9885a56ee4ecbc7c21
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-03 19:02:35 -0600

    24

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetForm from "./components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
 import ExpenseModal from "./components/ExpenseModal"
+import ExpenseList from "./components/ExpenseList"
 
 function App() {  
   const { state } = useBudget()
@@ -22,11 +23,9 @@ function App() {
 
       {isValidBudget && (
         <main className="max-w-3xl mx-auto py-10">
-
-
+          <ExpenseList />
           <ExpenseModal />
         </main>
-        
       )}
     </>
   )
