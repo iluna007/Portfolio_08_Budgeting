b6bde3b16616e3b7cf6b1d47fbca4c44ba25db31
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-05 14:23:22 -0600

    32

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -70,7 +70,7 @@ export default function ExpenseForm() {
         <form className="space-y-5" onSubmit={handleSubmit}>
             <legend
                 className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
-            >Nuevo Gasto</legend>
+            >{state.editingId ? 'Guardar Cambios' : 'Nuevo Gasto'}</legend>
 
             {error && <ErrorMessage>{error}</ErrorMessage>}
 
@@ -144,7 +144,7 @@ export default function ExpenseForm() {
             <input
                 type="submit"
                 className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
-                value={'Registrar Gasto'}
+                value={state.editingId ? 'Guardar Cambios' : 'Registrar Gasto'}
             />
         </form>
     )
