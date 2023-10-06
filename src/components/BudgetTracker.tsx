c1e4cee83e17327302523762b107d50d45d182e2
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-05 18:15:59 -0600

    34

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,36 +1,42 @@
+import { useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
 
-
 export default function BudgetTracker() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <div className="flex justify-center">
-            <img src="/grafico.jpg" alt="Gráfica de gastos" />
+    const { state } = useBudget()
+
+    const totalExpenses = useMemo( () => state.expenses.reduce((total, expense) => expense.amount + total, 0) , [state.expenses])
+    const remainingBudget = state.budget - totalExpenses
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <div className="flex justify-center">
+                <img src="/grafico.jpg" alt="Gráfica de gastos" />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-8">
+                <button
+                    type="button"
+                    className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+                >
+                    Resetear App
+                </button>
+
+                <AmountDisplay
+                    label="Presupuesto"
+                    amount={state.budget}
+                />
+
+                <AmountDisplay
+                    label="Disponible"
+                    amount={remainingBudget}
+                />
+
+                <AmountDisplay
+                    label="Gastado"
+                    amount={totalExpenses}
+                />
+            </div>
         </div>
-
-        <div className="flex flex-col justify-center items-center gap-8">
-            <button
-                type="button"
-                className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
-            >
-                Resetear App
-            </button>
-
-            <AmountDisplay
-                label="Presupuesto"
-                amount={300}
-            />
-
-            <AmountDisplay
-                label="Disponible"
-                amount={200}
-            />
-
-            <AmountDisplay
-                label="Gastado"
-                amount={100}
-            />
-        </div>
-    </div>
-  )
+    )
 }
